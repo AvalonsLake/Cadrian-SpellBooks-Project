ecc1d72a6352d30cbb97b2c4e3e8e49fc2158367
AvalonsLake <avalonslake@gmail.com>
2025-07-23 13:05:55 -0600

    list functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* Template
+  {
     name: "",
     level: ,
     plane: ,
@@ -11,11 +12,12 @@
     components: ,
     duration: ,
     description: ,
+  },
     */
 
 // Arcane Spells
-let arcane = {
-  arcaneBlast: {
+const arcane = [
+  {
     name: "Arcane Blast",
     level: 0,
     plane: "Arcane",
@@ -30,12 +32,12 @@ let arcane = {
     description:
       "You create a ball of arcane energy in your hands and fire it at a creature you can see within range. Make an *Arcane Attack*, on a hit you deal 1d6 + *Mind Score* of force damage.",
   },
-  arcaneHand: {
+  {
     name: "Arcane Hand",
     level: 0,
     manaCost: 0,
     plane: "Arcane",
-    ritual: false,
+    ritual: "",
     castTime: "Action",
     range: "60ft",
     type: "Utility",
@@ -45,11 +47,11 @@ let arcane = {
     description:
       "You create a spectral hand that you can control with your mind by using your bonus action each turn (it has a flying speed equal to your movement speed). It can lift up to 5 pounds, and if it is used to attack a creature, make an *Arcane Attack* as a bonus action, on a hit the target takes damage equal to your *Mind Score*.",
   },
-  manipulate: {
+  {
     name: "Manipulate",
     level: 0,
     plane: "Arcane",
-    ritual: false,
+    ritual: "",
     manaCost: 0,
     castTime: "Action",
     range: "30ft",
@@ -60,11 +62,11 @@ let arcane = {
     description:
       "You are able to manipulate things with your mind. Choose one of the following affects to have happen when to cast the spell:- You purge an area that can fit within 1 cubic foot or 10 square feet of all filth and debris, it can clean a creature's body and clothing.- You wave your hand over inorganic material and form it into a ball that can fit into your hand. The ball has a thin layer of magic that holds it together so you can make a ball out of liquid as well.- You cause an unlocked door or window you can swee within range to open or close. Or you cause a tiny object to move up to 5 feet.- You create a noise or a voice saying up to 5 words at a point within range.",
   },
-  siphon: {
+  {
     name: "Siphon",
     level: 0,
     plane: "Eldritch",
-    ritual: false,
+    ritual: "",
     manaCost: 0,
     castTime: "Action",
     range: "Touch",
@@ -75,11 +77,11 @@ let arcane = {
     description:
       "You touch a willing creature and either give or receive mana from them, but it can not exceed a creature's maximum number of mana (this doesn't count toward expending mana for focusing purposes).",
   },
-  stabalize: {
+  {
     name: "Stabalize",
     level: 0,
     plane: "Arcane",
-    ritual: false,
+    ritual: "",
     manaCost: 0,
     castTime: "Action",
     range: "Self",
@@ -90,11 +92,11 @@ let arcane = {
     description:
       "You stabilize the ambient magic in the area to make it easier to cast ritual spells. The next ritual spell that is cast within 15 feet of you has its casting time reduced by a number of rounds equal to your *Mind Score*. If a spell is cast while in the area (that isn’t a ritual being performed), the effects of this spell are lost due to the ambient magic being destabilized.",
   },
-  return: {
+  {
     name: "Return",
     level: 0,
     plane: "Arcane",
-    ritual: false,
+    ritual: "",
     manaCost: 0,
     castTime: "Action",
     range: "60ft",
@@ -105,32 +107,32 @@ let arcane = {
     description:
       "You stretch out your hand towards an object that you can see within range, that is not being worn or carried, and is no heavier than 5 pounds, and it instantly appears in your hand.",
   },
-};
-let dragon = {};
-let space = {};
-let time = {};
-let ethereal = {};
-let eldritch = {};
+];
+const dragon = [];
+const space = [];
+const time = [];
+const ethereal = [];
+const eldritch = [];
 
 // Faith Spells
-let heavens = {};
-let judgement = {};
-let abyss = {};
-let hells = {};
-let divine = {};
-let everWar = {};
-let various = {};
+const heavens = [];
+const judgement = [];
+const abyss = [];
+const divine = [];
+const everWar = [];
+const various = [];
 
 // Nature Spells
-let air = {};
-let earth = {};
-let fire = {};
-let water = {};
-let nature = {};
-let shadowLands = {};
+const air = [];
+const earth = [];
+const fire = [];
+const water = [];
+const nature = [];
+const shadowLands = [];
 
-let aura = {};
+let aura = [];
 let featuredSpell;
+let displayedSpell = 0;
 
 let spellName = document.getElementById("spellName");
 let spellLevel = document.getElementById("spellLevel");
@@ -143,19 +145,37 @@ let spellType = document.getElementById("spellType");
 let spellComponents = document.getElementById("spellComponents");
 let spellDuration = document.getElementById("spellDuration");
 let spellDescription = document.getElementById("spellDescription");
+let arcaneList = document.getElementById("arcaneShelf");
 
-function displaySpell() {
-  spellName.innerText = arcane.arcaneBlast.name;
-  spellLevel.innerText = `Level: ${arcane.arcaneBlast.level}`;
-  spellPlane.innerText = arcane.arcaneBlast.plane;
-  spellRitual.innerText = arcane.arcaneBlast.ritual;
-  spellCost.innerText = `Mana Cost: ${arcane.arcaneBlast.manaCost}`;
-  spellCastTime.innerText = `Cast Time: ${arcane.arcaneBlast.castTime}`;
-  spellRange.innerText = `Range: ${arcane.arcaneBlast.range}`;
-  spellType.innerText = `Type: ${arcane.arcaneBlast.type}`;
-  spellComponents.innerText = `Components: ${arcane.arcaneBlast.components}`;
-  spellDuration.innerText = `Duration: ${arcane.arcaneBlast.duration}`;
-  spellDescription.innerText = arcane.arcaneBlast.description;
+function generateShelf() {
+  let length = arcane.length;
+  for (let i = 0; i <= length; i++) {
+    let p = document.createElement("p");
+    p.innerText = `${arcane[i].plane} Plane --- || --- ${arcane[i].name} --- || --- Level: ${arcane[i].level}`;
+    p.value = i;
+    p.addEventListener("click", () => {
+      displayedSpell = p.value;
+      displaySpell();
+    });
+    arcaneList.appendChild(p);
+  }
 }
 
+function displaySpell() {
+  spellName.innerText = arcane[displayedSpell].name;
+  spellLevel.innerText = `Level: ${arcane[displayedSpell].level}`;
+  spellPlane.innerText = arcane[displayedSpell].plane;
+  spellRitual.innerText = arcane[displayedSpell].ritual;
+  spellCost.innerText = `Mana Cost: ${arcane[displayedSpell].manaCost}`;
+  spellCastTime.innerText = `Cast Time: ${arcane[displayedSpell].castTime}`;
+  spellRange.innerText = `Range: ${arcane[displayedSpell].range}`;
+  spellType.innerText = `Type: ${arcane[displayedSpell].type}`;
+  spellComponents.innerText = `Components: ${arcane[displayedSpell].components}`;
+  spellDuration.innerText = `Duration: ${arcane[displayedSpell].duration}`;
+  spellDescription.innerText = arcane[displayedSpell].description;
+}
+
+function flipCard() {}
+
 displaySpell();
+generateShelf();
